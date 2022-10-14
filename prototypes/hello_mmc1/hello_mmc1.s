73b3013910cb68bd054aca9e4700c5383eb6a134
Alex Thorlton <bxlentpartyon@gmail.com>
2022-10-14 18:55:31 -0500

    prototypes/hello_mmc1: String drawing with mmc1.

--- prototypes/hello_mmc1/hello_mmc1.s
+++ prototypes/hello_mmc1/hello_mmc1.s
@@ -13,31 +13,64 @@
 	.byte 0, 0			; 14-15 Misc ROM, expansion device
 
 .segment "BANK00"
-	.res $3ff0
+	.byte $69, $00
 
 .segment "BANK01"
-	.res $3ff0
+	.byte $69, $01
 
 .segment "BANK02"
-	.res $3ff0
+	.byte $69, $02
 
 .segment "CODE"
+
+hello_str_end:	.byte 0
+hello_str:	.byte $2f, $2c, $2c, $25, $28 ; backwards to work with dex
+hello_str_len:	.byte 5
+
 .proc reset
 	sei
 	cld
 	ldx #$40
 	stx $4017
-	ldx #$ff
+
+	ldx #$ff 	; set stack pointer to 0x01ff
 	txs
-	inx
-	stx $2000
-	stx $2001
-	stx $4010
+
+	inx		; X = 0
+	stx $2000	; disable vblank NMI
+	stx $2001	; disable rendering
+	stx $4010	; disable DMC IRQ
+
+	bit $2002	; acknowledge stray vblank
 
 vblankwait1:
 	bit $2002
 	bpl vblankwait1
 
+;setup_mc:
+;	lda #$0f
+;	sta $8000
+;	lsr
+;	sta $8000
+;	lsr
+;	sta $8000
+;	lsr
+;	sta $8000
+;	lsr
+;	sta $8000
+;
+;set_chr_bank:
+;	lda #$00	; set CHR to bank 0
+;	sta $a000
+;	lsr
+;	sta $a000
+;	lsr
+;	sta $a000
+;	lsr
+;	sta $a000
+;	lsr
+;	sta $a000
+;
 clrmem:
 	lda #$00
 	sta $0000, x
@@ -55,6 +88,35 @@ vblankwait2:
 	bit $2002
 	bne vblankwait2
 
+set_palette:
+	lda #$3f
+	sta $2006
+	lda #$01
+	sta $2006
+	lda #$30
+	sta $2007
+
+put_str:
+	lda #$23
+	sta $2006
+	lda #$80
+	sta $2006
+	ldx hello_str_len
+put_str_loop:
+	lda hello_str_end, x
+	sta $2007
+	dex
+	bne put_str_loop
+
+ppu_on:
+	lda #0
+	sta $2005
+	sta $2005
+	ldx #$00
+	stx $2000
+	ldx #$0a
+	stx $2001
+
 forever:
 	jmp forever
 .endproc
@@ -70,7 +132,8 @@ stub_entry:
 	sei
 	ldx #$ff
 	txs
-	stx $fff2	; reset mmc1
+	stx $8000	; reset mmc1
+
 	jmp reset
 	.addr nmi, stub_entry, 0
 .endscope
@@ -82,4 +145,4 @@ reset_stub "STUB02"
 reset_stub "STUB03"
 
 .segment "CHR"
-	.incbin "empty.chr"
+	.incbin "tileset.chr"
