b023652dc616467c24b8123949e5f1ab109aa237
Alex Thorlton <bxlentpartyon@gmail.com>
2023-05-04 21:51:42 -0500

    WIP buffer dump works once
    The buffer dump loop isn't resetting the buffer positions when it
    should, but it's writing the correct stuff one time at least.  The putc
    function still isn't working.

--- prototypes/uzi-nes-port/kernel.c
+++ prototypes/uzi-nes-port/kernel.c
@@ -1,35 +1,56 @@
-extern void dump_screenbuf(void);
+extern void __dump_screenbuf(void);
 extern void ppu_reset(void);
+extern void ei(void);
 
-#define SCREEN_BUF_SIZE 10
-char screenbuf[10] = { 'f', 'u', 'c', 'k', 0, 0, 0, 0, 0, 0 };
-char *screenbuf_chunk1 = screenbuf;
-char *screenbuf_chunk2 = screenbuf + 240;
-char *screenbuf_chunk3 = screenbuf + 480;
-char *screenbuf_chunk4 = screenbuf + 720;
-int bufpos = 0;
+extern int buffer_dump_pos;
+extern int ppu_dump_pos;
+extern char *screenbuf;
+
+#pragma zpsym ("buffer_dump_pos")
+#pragma zpsym ("ppu_dump_pos")
+
+#define SCREEN_BUF_SIZE 960
+//char screenbuf[10] = { 'f', 'u', 'c', 'k', 0, 0, 0, 0, 0, 0 };
+int cursor_pos = 0;
 
 void putc(char c)
 {
 	char ptable_c = c - 0x20;
-	screenbuf[bufpos] = ptable_c;
-	//screenbuf[bufpos] = c;
-	bufpos += 1;
+	screenbuf[cursor_pos] = ptable_c;
+	cursor_pos += 1;
 
-	if (bufpos > SCREEN_BUF_SIZE)
-		bufpos = 0;
+	if (cursor_pos >= SCREEN_BUF_SIZE)
+		cursor_pos = 0;
 
 	return;
 }
 
 void start_kernel(void)
 {
+	buffer_dump_pos = &screenbuf;
+	ppu_dump_pos = (char *) 0x2020;
+
 	putc('H');
 	putc('E');
 	putc('L');
 	putc('L');
 	putc('O');
+
+	ei();
+
 	while (1);
+}
+
+void dump_screenbuf(void)
+{
+	__dump_screenbuf();
+
+	buffer_dump_pos = buffer_dump_pos + 10;
+	ppu_dump_pos = ppu_dump_pos + 10;
+	if (buffer_dump_pos >= buffer_dump_pos + SCREEN_BUF_SIZE) {
+		buffer_dump_pos = &screenbuf;
+		ppu_dump_pos = 0x2020;
+	}
 }
 
 void handle_vblank(void)
