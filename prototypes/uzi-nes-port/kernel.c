e2ad968b39331ea93ab01b3a86417be629aaa842
Alex Thorlton <bxlentpartyon@gmail.com>
2023-05-16 22:26:01 -0500

    kernel.c: Switch to += for loop increments in dump_screenbuf()
    I'm not sure why I didn't trust += to work initially, but it's clearly
    working fine this way.

--- prototypes/uzi-nes-port/kernel.c
+++ prototypes/uzi-nes-port/kernel.c
@@ -44,8 +44,8 @@ void dump_screenbuf(void)
 {
 	__dump_screenbuf();
 
-	buffer_dump_pos = buffer_dump_pos + 16;
-	ppu_dump_pos = ppu_dump_pos + 16;
+	buffer_dump_pos += 16;
+	ppu_dump_pos += 16;
 
 	if (buffer_dump_pos == ((char *) &screenbuf) + SCREEN_BUF_SIZE) {
 		buffer_dump_pos = (char *) &screenbuf;
