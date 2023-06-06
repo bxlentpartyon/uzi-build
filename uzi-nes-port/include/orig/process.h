bf409fbc89a397c48398377c30628193b9c35871
Alex Thorlton <bxlentpartyon@gmail.com>
2023-06-05 22:15:08 -0500

    include/process.h: Move unused code to orig/ subdir

---
+++ uzi-nes-port/include/orig/process.h
@@ -0,0 +1,14 @@
+void psleep(void *event);
+void sendsig(ptptr proc, int16 sig);
+void wakeup(char *event);
+void ssig(register ptptr proc, int16 sig);
+void chksigs(void);
+void swapin(ptptr pp);
+ptptr getproc(void);
+int dofork(void);
+int clk_int(void);
+
+/* only referenced inside process.c */
+void newproc(ptptr p);
+int swapout(void);
+void swrite(void);
--- uzi-nes-port/include/process.h
+++ uzi-nes-port/include/process.h
@@ -1,24 +1,6 @@
 #ifndef __PROCESS_H__
 #define __PROCESS_H__
 
-#if 0
-void psleep(void *event);
-void sendsig(ptptr proc, int16 sig);
-void wakeup(char *event);
-void ssig(register ptptr proc, int16 sig);
-void chksigs(void);
-void swapin(ptptr pp);
-ptptr getproc(void);
-int dofork(void);
-#endif
 void init2(void);
-#if 0
-int clk_int(void);
-
-/* only referenced inside process.c */
-void newproc(ptptr p);
-int swapout(void);
-void swrite(void);
-#endif
 
 #endif /* __PROCESS_H__ */
