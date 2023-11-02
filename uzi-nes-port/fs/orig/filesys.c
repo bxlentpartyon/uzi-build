a755f8601d3b3f51c238dff1afc719cafe2ce5c9
Alex Thorlton <bxlentpartyon@gmail.com>
2023-11-02 00:26:17 -0500

    filesys.c: Unstub i_deref
    This still fails to build, but the code is here now.

--- uzi-nes-port/fs/orig/filesys.c
+++ uzi-nes-port/fs/orig/filesys.c
@@ -672,40 +672,6 @@ int uf_alloc(void)
     return(-1);
 }
 
-/* I_deref decreases the reference count of an inode, and frees it
-from the table if there are no more references to it.  If it also
-has no links, the inode itself and its blocks (if not a device) is freed. */
-
-void i_deref(register inoptr ino)
-{
-    magic(ino);
-
-    ifnot (ino->c_refs)
-	panic("inode freed.");
-
-    if ((ino->c_node.i_mode_hi & F_MASK) == F_PIPE)
-	wakeup((char *)ino);
-
-    /* If the inode has no links and no refs, it must have
-    its blocks freed. */
-
-    ifnot (--ino->c_refs || ino->c_node.i_nlink)
-	    f_trunc(ino);
-
-    /* If the inode was modified, we must write it to disk. */
-    if (!(ino->c_refs) && ino->c_dirty)
-    {
-	ifnot (ino->c_node.i_nlink)
-	{
-	    ino->c_node.i_mode_lo = 0;
-	    ino->c_node.i_mode_hi = 0;
-	    i_free(ino->c_dev, ino->c_num);
-	}
-	wr_inode(ino);
-    }
-}
-
-
 /* Wr_inode writes out the given inode in the inode table out to disk,
 and resets its dirty bit. */
 
