7146c4c680d568c677a7a3e828cb506de0379851
Alex Thorlton <bxlentpartyon@gmail.com>
2024-08-21 23:05:00 -0500

    uzi-nes-utils: Initial fsutil commit
    This will eventually be a shell-ish utility to manipulate an UZI
    filesystem image from a Linux system.  There's not much to it yet.  Just
    checkpointing it here, since I'm tired, and it's building and "working,"
    in that it doesn't crash.

---
+++ uzi-nes-utils/src/fsutil.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "linux_fs.h"
+
+struct filesys *filsys;
+char sb_buf[DUMMY_BLOCKSIZE] =  { 0 };
+
+#define error(...)	fprintf(stderr, "error: " __VA_ARGS__);
+
+void usage(void)
+{
+	fprintf(stderr, "usage: fsutil [-h] fs_image\n");
+}
+
+int main(int argc, char **argv)
+{
+	int opt, fd;
+	char *fs_image;
+
+	while ((opt = getopt(argc, argv, "h")) != -1) {
+		switch(opt) {
+		case 'h':
+			usage();
+			exit(0);
+		default:
+			usage();
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (optind >= argc) {
+		error("Expected fs_image argument after options\n");
+		usage();
+		exit(EXIT_FAILURE);
+	}
+
+	fs_image = argv[optind];
+	printf("Mounting UNIX filesystem image: %s\n", fs_image);
+
+	fd = open(fs_image, O_RDWR);
+	if (fd < 0) {
+		error("Can't open file %s\n", fs_image);
+		exit(EXIT_FAILURE);
+	}
+
+	close(fd);
+
+	return EXIT_SUCCESS;
+}
