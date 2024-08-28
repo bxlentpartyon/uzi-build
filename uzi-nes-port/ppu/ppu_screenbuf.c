f34d49f57e6f40cf4a8e503da98a2594e942e75a
Alex Thorlton <bxlentpartyon@gmail.com>
2024-08-27 21:07:52 -0500

    ppu: Move screenbuf C code to its own file
    This is a continuation of the previous commit.  I really think the ASM
    code should be moved to a subdir instead of just having an _asm suffix,
    but this is good enough for now.

---
+++ uzi-nes-port/ppu/ppu_screenbuf.c
@@ -0,0 +1,73 @@
+#include <ppu.h>
+
+extern char *buffer_dump_pos;
+extern char *ppu_dump_pos;
+#pragma zpsym ("buffer_dump_pos")
+#pragma zpsym ("ppu_dump_pos")
+
+extern char screenbuf[];
+
+#define SCREEN_VIS_ROWS		28
+#define SCREEN_COLS		32
+#define SCREEN_BUF_SIZE		SCREEN_VIS_ROWS * SCREEN_COLS
+#define SCREEN_BUF_START	(char *) &screenbuf
+
+#define PPU_FIRST_VIS_ROW	(char *) 0x2020;
+
+int cursor_pos = 0;
+int in_panic = 0;
+
+void next_line(void)
+{
+	int chars_left, i;
+
+	chars_left = SCREEN_COLS - (cursor_pos % SCREEN_COLS);
+
+	for (i = 0; i < chars_left; i++)
+		screenbuf[cursor_pos++] = '*';
+}
+
+void ppu_putc(char c)
+{
+	if (c == '\r') {
+		next_line();
+	} else {
+		screenbuf[cursor_pos++] = c;
+	}
+
+	BUG_ON(cursor_pos > SCREEN_BUF_SIZE);
+
+	if (cursor_pos == SCREEN_BUF_SIZE)
+		cursor_pos = 0;
+
+	return;
+}
+
+void ppu_puts(char *s)
+{
+	int pos = 0;
+	char cur_char = *s;
+
+	while (cur_char != 0x00) {
+		ppu_putc(cur_char);
+		cur_char = s[++pos];
+	}
+}
+
+void panic(char *msg)
+{
+	/* prevent recursion from ppu_put* functions */
+	if (!in_panic) {
+		in_panic = 1;
+		cursor_pos = 0;
+		ppu_puts("PANIC: ");
+		ppu_puts(msg);
+		while(1);
+	}
+}
+
+void ppu_init_screenbuf(void)
+{
+	buffer_dump_pos = SCREEN_BUF_START;
+	ppu_dump_pos = PPU_FIRST_VIS_ROW;
+}
