4be95a3c86c75a84c5970762193550652acf23e0
Alex Thorlton <bxlentpartyon@gmail.com>
2024-09-23 21:50:58 -0500

    WIP trying to implement multi-byte writes
    This code is a good start (maybe), but I'm realizing that I really need
    to fix BG scrolling before implementing this, as having a potential
    screen-wrap occur in the middle of a descriptor is an annoying corner
    case to code for, and it's something I really don't want to have happen
    in the end, no matter what.

--- uzi-nes-port/ppu/ppu_databuf.c
+++ uzi-nes-port/ppu/ppu_databuf.c
@@ -2,40 +2,33 @@
 #include <ppu.h>
 #include <lib/string.h>
 
-#define	PPU_MAX_WRITE	16	// Max number of bytes we can write to the PPU in one VBlank
-#define PPU_BUF_BYTE	5	// Write buffer entry size for a single byte
-#define PPU_BUF_SIZE	PPU_MAX_WRITE * PPU_BUF_BYTE
+#define	PPU_MAX_WRITE		16				// Max number of bytes we can write to the PPU in one VBlank
+#define PPU_BUF_BYTE		sizeof(struct ppu_desc) + 1	// Write buffer entry size for a single byte
+#define PPU_BUF_SIZE		PPU_MAX_WRITE * PPU_BUF_BYTE
+#define PPU_MAX_DESC_BYTES	16
+#define NULL_BYTE_SIZE		1
 
 extern char ppu_databuf[];
 extern int databuf_pos;
 extern void wait_frame(void);
 
+char ppu_descbuf[PPU_MAX_DESC_BYTES];
 char *cur_screen_ptr = PPU_FIRST_VIS_ROW;
+char ppu_descbuf_pos = 0;
 
-void next_line(void)
-{
-	int chars_left;
+#define CUR_DESC_BYTES	ppu_descbuf_pos + 1
+#define CUR_DESC_SIZE	sizeof(struct ppu_desc) + CUR_DESC_BYTES 
 
-	chars_left = SCREEN_COLS - ((unsigned int) cur_screen_ptr % SCREEN_COLS);
-	cur_screen_ptr += chars_left;
-}
-
-void ppu_putc(char c)
+void dump_current_descriptor(void)
 {
 	struct ppu_desc desc;
 
-	if (c == '\r') {
-		next_line();
-		return;
-	}
+	if (databuf_pos + CUR_DESC_SIZE + NULL_BYTE > PPU_BUF_SIZE)
+		wait_frame();
 
 	desc.size = 1;
 	desc.target = cur_screen_ptr;
 	desc.flags = 0;
-	desc.data = c;
-
-	if (databuf_pos + sizeof(struct ppu_desc) + 1 > PPU_BUF_SIZE)
-		wait_frame();
 
 	/*
 	 * We need to disable interrupts to avoid having a vblank occur with a
@@ -45,12 +38,35 @@ void ppu_putc(char c)
 
 	bcopy(&desc, ppu_databuf + databuf_pos, sizeof(struct ppu_desc));
 	databuf_pos += sizeof(struct ppu_desc);
+	bcopy(ppu_descbuf, ppu_databuf + databuf_pos, CUR_DESC_BYTES);
+	databuf_pos += CUR_DESC_BYTES;
 	ppu_databuf[databuf_pos] = 0;
-
-	if (++cur_screen_ptr > PPU_FIRST_VIS_ROW + SCREEN_BUF_SIZE)
+	
+	if (cur_screen_ptr + CUR_DESC_BYTES > PPU_VIS_END)
 		cur_screen_ptr = PPU_FIRST_VIS_ROW;
 
 	ei();
+}
+
+void next_line(void)
+{
+	int chars_left = SCREEN_COLS - ((unsigned int) cur_screen_ptr % SCREEN_COLS);
+
+	/* If we're on the last visible row, wrap around */
+	if (cur_screen_ptr + chars_left < PPU_VIS_END)
+		cur_screen_ptr += chars_left;
+	else
+		cur_screen_ptr = PPU_FIRST_VIS_ROW;
+}
+
+void ppu_putc(char c)
+{
+	if (c == '\r') {
+		next_line();
+		return;
+	}
+
+	if (CUR_DESC_BYTES + 1 > PPU_MAX_DESC_BYTES
 }
 
 /*
