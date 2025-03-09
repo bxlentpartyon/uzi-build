9a692880e2fab14caeafdc1a5bd495e9e32306e8
Alex Thorlton <bxlentpartyon@gmail.com>
2025-03-09 01:01:28 -0600

    utils/fs/fsutil: Pull in ls code from upc
    This is the ls function from Doug's upc program.

--- uzi-nes-utils/fs/fsutil_src/ls.c
+++ uzi-nes-utils/fs/fsutil_src/ls.c
@@ -4,7 +4,51 @@
 
 int ls(char *path)
 {
-	printf("debug %s\n", path);
+    struct direct buf;
+    struct stat statbuf;
+    char dname[128];
+    int d;
+
+    d = _open(path,0);
+    if (d < 0)
+    {
+	printf("ls: can't open %s\n",path);
+	return;
+    }
+
+    while (_read(d,(char *)&buf,16) == 16)
+    {
+	if (buf.d_name[0] == '\0')
+	    continue;
+
+	if (path[0] != '.' || path[1] )
+	{
+	    strcpy(dname,path);
+	    strcat(dname,"/");
+	}
+	else
+	    dname[0] = '\0';
+	strcat(dname,buf.d_name);
+
+	if (_stat(dname,&statbuf) != 0)
+	{
+	    printf("ls: can't stat %s\n",dname);
+	    continue;
+	}
+
+	if ((statbuf.st_mode & F_MASK) == F_DIR)
+	    strcat(dname,"/");
+
+	printf("%-6d %-15s",
+		(statbuf.st_mode & F_CDEV) ?
+		    statbuf.st_rdev :
+		    512*statbuf.st_size.o_blkno + statbuf.st_size.o_offset,
+		dname);
+
+	printf("  0%-6o %-2d %-5d\n", statbuf.st_mode, statbuf.st_nlink,
+		statbuf.st_ino);
+    }
+    _close(d);
 }
 
 int ls_main(int argc, char **argv)
