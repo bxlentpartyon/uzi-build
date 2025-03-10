a89fa2fd29ffbb383bddf4c27da036602131950c
Alex Thorlton <bxlentpartyon@gmail.com>
2025-03-10 15:34:39 -0500

    utils/fs/fsutil: Print ctime as part of ls
    This code came from the uzi280clib source, but I'm pasting it here for
    now since I don't have any C library started.

--- uzi-nes-utils/fs/fsutil_src/fsutil_ls.c
+++ uzi-nes-utils/fs/fsutil_src/fsutil_ls.c
@@ -2,6 +2,38 @@
 
 #include "fsutil.h"
 
+static char *s;
+
+void dout(int n)
+{
+	n %= 100;
+	*s++ = n/10 + '0';
+	*s++ = n%10 + '0';
+}
+
+char *ctime(time_t *t)
+{
+	static char str[24];
+
+	s = str;
+	*s = '\0';
+
+	dout((t->t_time&0xf800)>>11);
+	*s++ = ':';
+	dout((t->t_time&0x07e0)>>5);
+	*s++ = ':';
+	dout(t->t_time&0x001f * 2);
+	*s++ = ' ';
+	dout((t->t_date&0x01e0)>>5);
+	*s++ = '/';
+	dout(t->t_date&0x001f);
+	*s++ = '/';
+	dout((t->t_date&0xfe00)>>9);
+	*s = '\0';
+
+	return(str);
+}
+
 int ls(char *path)
 {
 	struct direct buf;
@@ -40,8 +72,8 @@ int ls(char *path)
 		       512 * statbuf.st_size.o_blkno + statbuf.st_size.o_offset,
 		       dname);
 
-		fsutil_printf("  0%-6o %-2d %-5d\n", statbuf.st_mode, statbuf.st_nlink,
-		       statbuf.st_ino);
+		fsutil_printf("  0%-6o %-2d %-5d %s\n", statbuf.st_mode, statbuf.st_nlink,
+		       statbuf.st_ino, ctime(&statbuf.st_mtime));
 	}
 	_close(d);
 }
