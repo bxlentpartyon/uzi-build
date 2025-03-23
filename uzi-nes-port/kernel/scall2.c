f954717ed90414437a0192a12a19f1ebef40ef89
Alex Thorlton <bxlentpartyon@gmail.com>
2025-03-23 03:33:02 -0500

    kernel/scall2.c: Pull in _mount/_umount
    This is the last chunk of stuff in this file, so remove it.

--- uzi-nes-port/kernel/scall2.c
+++ uzi-nes-port/kernel/scall2.c
@@ -130,4 +130,157 @@ int _ioctl(int fd, int request, char *data)
 #undef data
 */
 
+/* This implementation of mount ignores the rwflag */
+
+/*****************************************
+mount(spec, dir, rwflag)
+char *spec;
+char *dir;
+int rwflag;
+*******************************************/
+
+/*
+#define spec (char *)udata.u_argn2
+#define dir (char *)udata.u_argn1
+#define rwflag (int)udata.u_argn
+*/
+
+int _mount(char *spec, char *dir, int rwflag)
+{
+    register inoptr sino, dino;
+    register int dev;
+    inoptr n_open();
+
+    ifnot(super())
+    {
+	udata.u_error = EPERM;
+	return (-1);
+    }
+
+    ifnot (sino = n_open(spec,NULLINOPTR))
+	return (-1);
+
+    ifnot (dino = n_open(dir,NULLINOPTR))
+    {
+	i_deref(sino);
+	return (-1);
+    }
+
+    if (getmode(sino) != F_BDEV)
+    {
+	udata.u_error = ENOTBLK;
+	goto nogood;
+    }
+
+    if (getmode(dino) != F_DIR)
+    {
+	udata.u_error = ENOTDIR;
+	goto nogood;
+    }
+
+    dev = (int)sino->c_node.i_addr[0];
+
+    if ( dev >= NDEVS || d_open(dev))
+    {
+	udata.u_error = ENXIO;
+	goto nogood;
+    }
+
+    if (fs_tab[dev].s_mounted || dino->c_refs != 1 || dino->c_num == ROOTINODE)
+    {
+       udata.u_error = EBUSY;
+       goto nogood;
+    }
+
+    _sync();
+
+    if (fmount(dev,dino))
+    {
+       udata.u_error = EBUSY;
+       goto nogood;
+    }
+
+    i_deref(dino);
+    i_deref(sino);
+    return(0);
+
+nogood:
+    i_deref(dino);
+    i_deref(sino);
+    return (-1);
+}
+
+/*
+#undef spec
+#undef dir
+#undef rwflag
+*/
+
+/******************************************
+umount(spec)
+char *spec;
+******************************************/
+
+/*
+#define spec (char *)udata.u_argn
+*/
+
+int _umount(char *spec)
+{
+    register inoptr sino;
+    register int dev;
+    register inoptr ptr;
+    inoptr n_open();
+
+    ifnot(super())
+    {
+	udata.u_error = EPERM;
+	return (-1);
+    }
+
+    ifnot (sino = n_open(spec,NULLINOPTR))
+	return (-1);
+
+    if (getmode(sino) != F_BDEV)
+    {
+	udata.u_error = ENOTBLK;
+	goto nogood;
+    }
+
+    dev = (int)sino->c_node.i_addr[0];
+    ifnot (validdev(dev))
+    {
+	udata.u_error = ENXIO;
+	goto nogood;
+    }
+
+    if (!fs_tab[dev].s_mounted)
+    {
+	udata.u_error = EINVAL;
+	goto nogood;
+    }
+
+    for (ptr = i_tab; ptr < i_tab+ITABSIZE; ++ptr)
+	if (ptr->c_refs > 0 && ptr->c_dev == dev)
+	{
+	    udata.u_error = EBUSY;
+	    goto nogood;
+	}
+
+    _sync();
+    fs_tab[dev].s_mounted = 0;
+    i_deref(fs_tab[dev].s_mntpt);
+
+    i_deref(sino);
+    return(0);
+
+nogood:
+    i_deref(sino);
+    return (-1);
+}
+
+/*
+#undef spec
+*/
+
 #pragma code-name (pop)
