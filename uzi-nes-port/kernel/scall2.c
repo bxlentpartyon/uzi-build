68436cdd77cd69d7ca0d5518552e461eab74238a
Alex Thorlton <alex.thorlton@oracle.com>
2025-04-13 22:56:04 -0500

    kernel/scall2.c: Fix post-program-load bzero
    Since progptr is pointing to the end of the first block of the program
    (i.e. 0x6200), I'm pretty sure this was causing us to do a bzero with
    size -0x200.  The size on bzero is unsigned, so I'm sure it was just
    interpreting this as some weirdly large value (0xfe00 I think?).
    
    Anyway, this is obviously wrong, so fix it.

--- uzi-nes-port/kernel/scall2.c
+++ uzi-nes-port/kernel/scall2.c
@@ -271,8 +271,9 @@ void exec2(void)
     }
     i_deref(udata.u_ino);
 
+
     /* Zero out the free memory */
-    bzero(progptr, (uint16) ((char *) MMC5_PRG_MODE3_BANK0_START - progptr));
+    bzero(progptr, (uint16) ((char *) MMC5_PRG_MODE3_BANK2_START - progptr));
     udata.u_break = progptr;
 
     /* Read back the arguments and the environment */
