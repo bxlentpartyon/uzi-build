f5f26acd4ba2dd748a6eb7e4147a9156b6e9a05e
Alex Thorlton <alex.thorlton@oracle.com>
2025-04-15 21:51:48 -0500

    kernel/scall2.c: Pull in _kill

--- uzi-nes-port/kernel/orig/scall2.c
+++ uzi-nes-port/kernel/orig/scall2.c
@@ -19,41 +19,6 @@ _fork()
    return (dofork());
 }
 
-/**************************************
-kill(pid, sig)
-int16 pid;
-int16 sig;
-*****************************************/
-
-#define pid (int16)udata.u_argn1
-#define sig (int16)udata.u_argn
-
-_kill()
-{
-    ptptr p;
-
-    if (sig <= 0 || sig > 15)
-	goto nogood;
-
-    for (p=ptab; p < ptab+PTABSIZE; ++p)
-    {
-	if (p->p_pid == pid)
-	{
-	    sendsig(p,sig);
-	    return(0);
-	}
-    }
-
-nogood:
-    udata.u_error = EINVAL;
-    return(-1);
-}
-
-#undef pid
-#undef sig
-
-
-
 /********************************
 alarm(secs)
 uint16 secs;
--- uzi-nes-port/kernel/scall2.c
+++ uzi-nes-port/kernel/scall2.c
@@ -597,4 +597,41 @@ nogood:
 #undef func
 */
 
+/**************************************
+kill(pid, sig)
+int16 pid;
+int16 sig;
+*****************************************/
+
+/*
+#define pid (int16)udata.u_argn1
+#define sig (int16)udata.u_argn
+*/
+
+int _kill(int16 pid, int16 sig)
+{
+    ptptr p;
+
+    if (sig <= 0 || sig > 15)
+	goto nogood;
+
+    for (p=ptab; p < ptab+PTABSIZE; ++p)
+    {
+	if (p->p_pid == pid)
+	{
+	    sendsig(p,sig);
+	    return(0);
+	}
+    }
+
+nogood:
+    udata.u_error = EINVAL;
+    return(-1);
+}
+
+/*
+#undef pid
+#undef sig
+*/
+
 #pragma code-name (pop)
