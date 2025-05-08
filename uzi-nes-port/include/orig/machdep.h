59935ff5bb9f4edbb9a6b36fa91f7058865072bf
Alex Thorlton <bxlentpartyon@gmail.com>
2025-05-07 23:11:04 -0500

    include/orig/machdep.h: Remove already-ported stuff

--- uzi-nes-port/include/orig/machdep.h
+++ uzi-nes-port/include/orig/machdep.h
@@ -1,9 +1,5 @@
 void idump(void);
 void doexec(void *new_stack);
-void calltrap(void);
-
-/* time functions */
-void rdtime(time_t *tloc);
 
 /* Not actually implemented yet */
 unsigned int in(unsigned int *addr);
