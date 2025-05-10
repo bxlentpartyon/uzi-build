3c47aa6cef5848fd4546c6d7137ddc1235af7a26
Alex Thorlton <bxlentpartyon@gmail.com>
2025-05-09 22:27:58 -0500

    kernel/process.c: Start porting unix()
    This won't port over cleanly because the indirect function calls don't
    play nice in cc65:
    
    <snip>
    kernel/process.c:439: Error: Undeclared identifier 'disp_tab'
    kernel/process.c:439: Error: Subscripted value is neither array nor pointer
    kernel/process.c:439: Error: Illegal indirection
    kernel/process.c:439: Error: Illegal function call
    </snip>
    
    For now just comment out the offending line, since we're going to have
    to handle the dispatch totally differently anyway, to account for all
    the banked calls.

--- uzi-nes-port/kernel/process.c
+++ uzi-nes-port/kernel/process.c
@@ -349,8 +349,6 @@ void swapin(ptptr pp)
     swapin_finish(udata.u_sp);
 }
 
-
-
 /* This sees if the current process has any signals set, and deals with them */
 void chksigs(void)
 {
@@ -414,8 +412,67 @@ void ssig(register ptptr proc, int16 sig)
 
 #pragma code-name (pop)
 
-/* Stub for unix().  Needs to be outside of PROCESS_CODE, in regular CODE */
-void unix(void)
+/*
+ * unix() needs to be outside of PROCESS_CODE, in regular CODE
+ * No auto vars here, so carry flag will be preserved
+ */
+int unix(int16 argn3, int16 argn2, int16 argn1, int16 argn,
+	 char* uret, int callno)
 {
-	return;
+    udata.u_argn3 = argn3;
+    udata.u_argn2 = argn2;
+    udata.u_argn1 = argn1;
+    udata.u_argn = argn;
+    udata.u_retloc = uret;
+    udata.u_callno = callno;
+
+    udata.u_insys = 1;
+    udata.u_error = 0;
+    ei();
+
+#ifdef DEBUG
+    kprintf ("\t\t\t\t\tcall %d (%x, %x, %x)\n",callno,argn2,argn1,argn);
+#endif
+
+    /* Branch to correct routine */
+    //udata.u_retval = (*disp_tab[udata.u_callno])();
+
+#ifdef DEBUG
+    kprintf("\t\t\t\t\t\tcall %d ret %x err %d\n",
+	udata.u_callno,udata.u_retval, udata.u_error);
+#endif
+
+
+    chksigs();
+    di();
+    if (runticks >= MAXTICKS)
+    {
+	udata.u_ptab->p_status = P_READY;
+	swapout();
+    }
+    ei();
+
+    udata.u_insys = 0;
+    calltrap();         /* Call trap routine if necessary */
+
+    /* If an error, return errno with carry set */
+
+    if (udata.u_error)
+    {
+	;
+
+/*
+#asm
+	LD      HL, (udata? + ?OERR)
+	POP     BC              ;restore frame pointer
+	PUSH    BC
+	POP     IX
+	SCF
+	RET
+#endasm
+*/
+	;
+    }
+
+    return(udata.u_retval);
 }
